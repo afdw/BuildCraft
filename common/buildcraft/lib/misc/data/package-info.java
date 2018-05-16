5768cd3f37cee4b6b1599953cd5ee8696eaacf8f
Anton Danilkin <afdw@yandex.ru>
2018-05-17 00:02:46 +0300

    Add missing annotations: main, manually

--- common/buildcraft/lib/misc/data/package-info.java
+++ common/buildcraft/lib/misc/data/package-info.java
@@ -7,4 +7,13 @@
 /** Provides a set of classes (data types or enums) that are useful in miscellaneous contexts. Unlike the parent
  * package, this is NOT a generic "all classes go here" - refer to more specific packages to find a more appropriate
  * data class for a particular use. */
+@FieldsAreNonNullByDefault
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 package buildcraft.lib.misc.data;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import buildcraft.api.FieldsAreNonNullByDefault;
+
+import mcp.MethodsReturnNonnullByDefault;
